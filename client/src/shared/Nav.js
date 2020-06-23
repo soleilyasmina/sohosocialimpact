2fb2ad62039b680803ede10da754888deb88db22
soleilyasmina <mounlasolomon@gmail.com>
2020-06-22 21:43:30 -0400

    about page and nav

--- client/src/shared/Nav.js
+++ client/src/shared/Nav.js
@@ -7,6 +7,9 @@ const Nav = () => (
     <Link to="/">Home</Link>
     <Link to="/about">About</Link>
     <a href="mailto:sohosocialimpact@gmail.com">Contact</a>
+    <a href="https://instagram.com/soho.social.impact">
+      <i class="fab fa-instagram"></i>
+    </a>
   </nav>
 );
 
